8755f7b23570591ab31cb220c43faeb4187dba43
yussak <yusuke.sakuraba0213@gmail.com>
2025-06-24 21:48:20 +0900

    コンテナエラー修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import todoRoutes from './routes/todos';
+import todoRoutes from './routes/todos.js';
 
 dotenv.config();
 
--- backend/src/routes/todos.ts
+++ backend/src/routes/todos.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import prisma from '../prisma';
+import prisma from '../prisma.js';
 
 const router = Router();
 
