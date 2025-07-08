66ed7bcf076f065301e348624911fffb8ee47b11
yussak <yusuke.sakuraba0213@gmail.com>
2025-07-08 23:36:26 +0900

    frontのテスト環境のセットアップ

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,7 +5,9 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
