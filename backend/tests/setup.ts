ae4b0cb057631ce475b622e017d2bf31d031da05
yussak <yusuke.sakuraba0213@gmail.com>
2025-07-19 22:45:36 +0900

    DATABASE_URLの廃止

--- backend/tests/setup.ts
+++ backend/tests/setup.ts
@@ -6,9 +6,23 @@ beforeAll(async () => {
   // rootのtestsでは一旦はDB接続のテストはしないのでbackendのみでチェックしている
 
   config({ path: ".env.test", override: true });
+
+  if (
+    // todo: DATABASE_URLは廃止(動的に作れるため)
+    process.env.DATABASE_URL == null &&
+    process.env.DB_USERNAME != null &&
+    process.env.DB_PASSWORD != null &&
+    process.env.DB_HOSTNAME != null &&
+    process.env.DB_DBNAME != null
+  ) {
+    process.env.DATABASE_URL = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:5432/${process.env.DB_DBNAME}`;
+  }
+
   const dbUrl = process.env.DATABASE_URL;
   if (!dbUrl) {
-    throw new Error("DATABASE_URLが設定されていません");
+    throw new Error(
+      "DATABASE_URLが設定されていません（または個別環境変数が不足）"
+    );
   }
 
   if (!dbUrl.includes("test_myapp")) {
