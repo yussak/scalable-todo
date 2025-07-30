e1f5f6189e5c67b4a209666543861c6fc1982f58
yussak <yusuke.sakuraba0213@gmail.com>
2025-07-31 00:23:03 +0900

    read .env.test

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { config } from "dotenv";
+
+config({ path: ".env.test" });
 
 export default defineConfig({
   plugins: [react()],
