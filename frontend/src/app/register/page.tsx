42ab93a124697dc741989e3a9a01991bb8706a87
yussak <yusuke.sakuraba0213@gmail.com>
2025-08-03 02:15:29 +0900

    fix lint error

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import api from "@/lib/api";
 import { useAuth } from "@/app/contexts/AuthContext";
